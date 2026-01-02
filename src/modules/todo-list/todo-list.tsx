2463630dd218ffb5214561fba7b2b74358b81664
azheevA <sazheev@gmail.com>
2026-01-02 22:57:36 +0800

    update infinity scroll

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,6 +1,6 @@
 import { keepPreviousData, useInfiniteQuery, useQuery } from "@tanstack/react-query"
 import { getTodoApi } from "../../shared/api/api"
-import { useState } from "react"
+import { useCallback, useRef, useState } from "react"
 
 export function TodoList(){
     
@@ -9,31 +9,64 @@ export function TodoList(){
         queryKey: ['tasks', 'list'],
         queryFn: (meta)=> getTodoApi.getTodoList({page: meta.pageParam},meta),
         initialPageParam: 1,
-        getNextPageParam: (result) => result.next
+        getNextPageParam: (result) => result.next,
+        select: result => result.pages.map(page => page.data).flat(),
         })
+     const cursorRef = useIntersection(()=>{
+        fetchNextPage()
+    })
+
     if(isLoading){ //status === "pending" && fetchStatus === "fetching"
-        return <div className="w-screen flex justify-center text-4xl">Loading</div>
+        return 
+        <div className="w-screen flex justify-center text-4xl">
+            Loading
+            </div>
     }    
     if(error){
         return <div>error: {JSON.stringify(error)}</div>
     }
+    
+
     return (
         <div className="w-screen flex flex-col items-center justify-center mx-auto">
             <button onClick={()=>setEnabled(e => !e)}>Toggle Enabled</button>
             <h1 className="text-3xl underline font-bold m-10">TodoList</h1>
                 <div className={"flex flex-col gap-5 " + (isPlaceholderData?'opacity-50':'')}>
-                    {todoItems?.data.map(todo => 
-                    <div key={todo.id} className="text-2xl text-[#9872c9] px-20 py-3  border-slate-500 border-2 rounded flex justify-center items-center ">
+                    {todoItems?.map(todo => 
+                    (<div key={todo.id} className="text-2xl text-[#9872c9] px-20 py-3  border-slate-500 border-2 rounded flex justify-center items-center ">
                         {todo.text}
-                        </div>)}
+                        </div>))}
+                        
                 </div>
-          
+                 <div className="flex gap-2 mt-4" ref={cursorRef}>
+                   {!hasNextPage && <div>Нета данных для загрузки</div>} 
+                   {isFetchingNextPage && <div>...Loading</div>}
+                </div>       
         
         </div>
     )
 }
 
+export function useIntersection(onIntersect: ()=> void){
 
+    const unsubsctibe = useRef(()=> {})
+
+    return useCallback((el:HTMLDivElement | null)=> {
+        const observer = new IntersectionObserver((entries)=>{
+            entries.forEach(intersection=>{
+                if(intersection.isIntersecting)
+                    onIntersect()
+            })
+        })
+
+        if(el){
+            observer.observe(el)
+            unsubsctibe.current = ()=> observer.disconnect()
+        }else{
+            unsubsctibe.current()
+        }
+    },[])
+}
 
 
 
